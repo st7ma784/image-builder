dfbff5451a3d51d6628002ad4a08931f9a2cf09a
Lane Aasen <laneaasen@gmail.com>
2020-07-22 11:29:37 -0700

    Allow setting workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-beaker config set user_token $INPUT_BEAKERTOKEN
+beaker config set user_token "$INPUT_BEAKERTOKEN"
+[[ ! -z "$INPUT_BEAKERWORKSPACE" ]] && beaker config set default_workspace "$INPUT_BEAKERWORKSPACE"
 
 TAG="beaker-image-builder"
-docker build . --file $INPUT_DOCKERFILE --tag $TAG
+docker build . --file "$INPUT_DOCKERFILE" --tag $TAG
 
 beaker image create $TAG
