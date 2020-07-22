48b76ec716cbe3fa59b1fd1923a8e06962e5c21c
Lane Aasen <laneaasen@gmail.com>
2020-07-22 12:13:39 -0700

    Fix image rename

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ beaker config set user_token "$INPUT_BEAKERTOKEN"
 TAG="beaker-image-builder"
 docker build . --file "$INPUT_DOCKERFILE" --tag $TAG
 
-IMAGE=$(beaker image create $TAG)
+IMAGE=$(beaker image create -q $TAG)
 
-[[ -n "$INPUT_IMAGENAME" ]] && beaker image rename $INPUT_IMAGENAME
+[[ -n "$INPUT_IMAGENAME" ]] && beaker image rename $IMAGE $INPUT_IMAGENAME
 
 echo "set-output name=imageID::$IMAGE"
