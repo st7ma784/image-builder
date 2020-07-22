ed42a20c02af0e720f36e8a1a088e62344fe7d79
Lane Aasen <laneaasen@gmail.com>
2020-07-22 11:57:29 -0700

    Allow setting image name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 beaker config set user_token "$INPUT_BEAKERTOKEN"
-[[ ! -z "$INPUT_BEAKERWORKSPACE" ]] && beaker config set default_workspace "$INPUT_BEAKERWORKSPACE"
+[[ -n "$INPUT_BEAKERWORKSPACE" ]] && beaker config set default_workspace "$INPUT_BEAKERWORKSPACE"
 
 TAG="beaker-image-builder"
 docker build . --file "$INPUT_DOCKERFILE" --tag $TAG
 
-beaker image create $TAG
+IMAGE=$(beaker image create $TAG)
+
+[[ -n "$INPUT_IMAGENAME" ]] && beaker image rename $INPUT_IMAGENAME
+
+echo "set-output name=imageID::$IMAGE"
