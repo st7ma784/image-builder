610828c4f8daeb74b01cab6d9e70ba1538a3f69c
Lane Aasen <laneaasen@gmail.com>
2020-07-22 12:31:12 -0700

    Cache docker builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,22 @@
 beaker config set user_token "$INPUT_BEAKERTOKEN"
 [[ -n "$INPUT_BEAKERWORKSPACE" ]] && beaker config set default_workspace "$INPUT_BEAKERWORKSPACE"
 
-TAG="beaker-image-builder"
-docker build . --file "$INPUT_DOCKERFILE" --tag $TAG
+TAG="docker.pkg.github.com/$GITHUB_REPOSITORY/beaker-image-build-cache"
+
+# Pull cached Docker image.
+if [[ -n "$INPUT_GITHUB_TOKEN" ]] then
+    # Login to GitHub Package registry.
+    echo $INPUT_GITHUB_TOKEN | docker login docker.pkg.github.com -u $GITHUB_ACTOR --password-stdin
+    # Pull existing Docker image from cache.
+    docker pull docker.pkg.github.com/$GITHUB_REPOSITORY/beaker-image-build-cache || true
+fi
+
+docker build . --file "$INPUT_DOCKERFILE" --tag "$TAG" --cache-from="$TAG"
+
+# Push Docker image to cache.
+if [[ -n "$INPUT_GITHUB_TOKEN" ]] then
+    docker push $TAG
+fi
 
 IMAGE=$(beaker image create -q $TAG)
 
