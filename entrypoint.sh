e7808c6106d6902613a1032759c97b3207222216
Lane Aasen <laneaasen@gmail.com>
2020-07-23 10:38:01 -0700

    Fix tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ beaker config set user_token "$INPUT_BEAKER_TOKEN"
 # The last section of the name must be globally unique, so add a hash of the repo name.
 TAG="docker.pkg.github.com/$GITHUB_REPOSITORY/beaker-image-build-cache-$(echo $GITHUB_REPOSITORY | md5sum)"
 # Docker repository names must be lowercase.
-TAG=$(echo  | tr '[:upper:]' '[:lower:]')
+TAG=$(echo $TAG | tr '[:upper:]' '[:lower:]')
 
 # Pull cached Docker image.
 if [[ -n "$INPUT_GITHUB_TOKEN" ]]
