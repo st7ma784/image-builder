57f094a3e780a3e87b88759df29b97660233604c
Stephen <37083603+st7ma784@users.noreply.github.com>
2021-09-28 14:51:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,8 @@ if [[ -n "$INPUT_GITHUB_TOKEN" ]]
 then
     docker push $TAG
 fi
-IMAGE="${INPUT_BEAKER_IMAGE_NAME:-$DEFAULT_NAME}"
+IMAGE=${INPUT_BEAKER_IMAGE_NAME:-$DEFAULT_NAME}
+#and failing that $Tag
 #IMAGE=$(echo $IMAGE | tr '[:upper:]' '[:lower:]')
 
 if [[ -n "$INPUT_SPEC_FILE" ]]
