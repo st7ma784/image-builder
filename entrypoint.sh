67f3645b5971d77d1c5a399c513f32036ef2153f
Stephen <37083603+st7ma784@users.noreply.github.com>
2021-10-04 21:18:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ beaker config set user_token "$INPUT_BEAKER_TOKEN"
 [[ -n "$INPUT_BEAKER_WORKSPACE" ]] && beaker config set default_workspace "$INPUT_BEAKER_WORKSPACE"
 
 # The last section of the name must be globally unique, so add a hash of the repo name.
-TAG="docker.io/$GITHUB_REPOSITORY-unitTest"
+TAG="$DOCKERFILE"
 #"docker.pkg.github.com/$GITHUB_REPOSITORY/beaker-image-build-cache-$(echo $GITHUB_REPOSITORY | md5sum | cut -d' ' -f1)"
 # Docker repository names must be lowercase.
 
@@ -21,15 +21,6 @@ then
     docker pull $TAG || true
 fi
 
-DOCKERFILE="$INPUT_DOCKERFILE"
-if [[ "$INPUT_DOCKERFILE_TEMPLATE" = "python-pip" ]]; then
-    DOCKERFILE="/dockerfiles/Dockerfile.python-pip"
-elif [[ -n "$INPUT_DOCKERFILE_TEMPLATE" ]]; then
-    echo "Invalid value for dockerfile_template"
-fi
-
-docker build . --file "$DOCKERFILE" --tag "$TAG" --cache-from="$TAG"
-
 DESC="https://github.com/$GITHUB_REPOSITORY/tree/$GITHUB_SHA"
 
 DEFAULT_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')-${GITHUB_SHA::7}
@@ -41,12 +32,6 @@ then
 fi
 
 beaker image create --description $DESC --name $NAME $TAG
-
-# Push Docker image to cache.
-if [[ -n "$INPUT_GITHUB_TOKEN" ]]
-then
-    docker push $TAG
-fi
 
 
 if [[ -n "$INPUT_SPEC_FILE" ]]
